3c2cf3315cb485e4c658e4ff24f40c70a3124484
Atara Razin <atararazin@gmail.com>
2019-05-27 15:38:02 +0300

    created a javascript file that draws a red dot at point (30,70)

---
+++ Ex3/Scripts/DrawLocation.js
@@ -0,0 +1,16 @@
+﻿function draw() {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    var canvas = document.getElementById('displayCanvas');
+    if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+
+        context.fillStyle = "#ff2626"; //red color
+        var xCoord = 30;
+        var yCoord = 70;
+        var pointSize = 4;
+        context.beginPath(); //Start path
+        context.arc(xCoord, yCoord, pointSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
+        context.fill();
+    }
+}
